060c9059dba5ea6de144ed7f01751a84eec0171a
Wamique Abro <wamik.abro212@gmail.com>
2024-10-03 16:05:53 +0500

    another way to export

--- Node JS/Modules/anotherModule.js
+++ Node JS/Modules/anotherModule.js
@@ -5,5 +5,9 @@ function helloModule(namee) {
 console.log(module); // see information about this module
 
 module.exports.helloModule = helloModule; // export helloModule function
+// we could've just exported the single function by replacing exports object
+// module.exports = helloModule
+// so to call it from the app.js, we will have to directly use the
+// variable thats holding it.
 
 console.log(module); // check exports in module information now.
