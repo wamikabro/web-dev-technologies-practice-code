8d32f56054fe1646363030de9f110dcecaecd9d7
Wamique Abro <wamik.abro212@gmail.com>
2024-10-03 12:50:10 +0500

    global objects

--- Node JS/app.js
+++ Node JS/app.js
@@ -5,3 +5,18 @@ function sayHello(name) {
 sayHello("Wamique");
 
 console.log(global); // global exists here instead of window
+
+global.setTimeout(() => {
+  console.log("setTimout ran.");
+}, 2000); // 2000ms = 2 seconds delay
+// or simply
+// setTimeout();
+
+var message = "I am stored locally.";
+
+// global doesn't have message, because variables are local
+// to the files where they're made
+// unlike browser's window that could access variables too.
+console.log(global.message);
+
+console.log(message); // we're accessing it locally
