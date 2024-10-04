b5baeb67b74d1c65671df6c50e7ebf0f42ef7718
Wamique Abro <wamik.abro212@gmail.com>
2024-10-04 17:37:41 +0500

    windows specific path and basename without exntension

--- Node JS/PathModule/app.js
+++ Node JS/PathModule/app.js
@@ -1,5 +1,12 @@
 import path from "node:path";
 
 const basename = path.basename("C:\\temp\\myfile.html");
+const specificWindowsBasename = path.win32.basename("C:\\temp\\myfile.html");
+const basenameWithoutExtension = path.win32.basename(
+  "C:\\temp\\myfile.html",
+  ".html"
+);
 
-console.log(basename);
+console.log("Basename: " + basename);
+console.log("Windows Specific Basename: " + specificWindowsBasename);
+console.log("Basename Without Extension: " + basenameWithoutExtension);
