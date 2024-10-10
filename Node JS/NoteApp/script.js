86ddc3c4c0c5f62cecf34b317dc71f3d003af691
Wamique Abro <wamik.abro212@gmail.com>
2024-10-10 19:12:20 +0500

    setup CLI, accept args, print them.

---
+++ Node JS/NoteApp/script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+let note = process.argv[2]; // after path and file name, the 3rd is the argument so argv[2]
+const newNote = {
+  id: Date.now(),
+  note,
+};
+
+console.log(newNote);
