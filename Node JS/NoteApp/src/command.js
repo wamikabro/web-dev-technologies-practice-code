117fa449d697b634871d23acb754e10cdda9dc33
Wamique Abro <wamik.abro212@gmail.com>
2024-10-12 08:57:53 +0500

    add first command and its flag

--- Node JS/NoteApp/src/command.js
+++ Node JS/NoteApp/src/command.js
@@ -3,12 +3,23 @@ import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
   .command(
-    "curl <url>",
-    "fetch the contents of the URL",
-    () => {},
-    (argv) => {
-      console.info(argv);
+    "new <note>",
+    "Create a New Note.",
+    (yargs) => {
+      return yargs.positional("note", {
+        type: "string",
+        description: "The content of the note to create",
+      });
+    },
+    (args) => {
+      console.log(args.note);
+      console.log(args.tags);
     }
   )
+  .option("tags", {
+    alias: "t",
+    type: "string",
+    description: "tags to add  to the note",
+  })
   .demandCommand(1)
   .parse();
