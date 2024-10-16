352f4dc4557aa9644dd74253f05f92c8c97532be
Wamique Abro <wamik.abro212@gmail.com>
2024-10-16 18:17:43 +0500

    write test for getAllNotes and removeNote

--- Node JS/NoteApp/tests/notes.test.js
+++ Node JS/NoteApp/tests/notes.test.js
@@ -37,3 +37,26 @@ test("newNote inserts data and returns it", async () => {
   expect(result.content).toEqual(note.content);
   expect(result.tags).toEqual(note.tags);
 });
+
+test("getAllNotes returns all notes", async () => {
+  const db = {
+    notes: ["note1", "note2", "note3"],
+  };
+  getDB.mockResolvedValue(db);
+
+  const result = await getAllNotes();
+  expect(result).toEqual(db.notes);
+});
+
+test("removeNote does nothing if id is not found", async () => {
+  const notes = [
+    { id: 1, content: "note 1" },
+    { id: 2, content: "note 2" },
+    { id: 3, content: "note 3" },
+  ];
+  saveDB.mockResolvedValue(notes);
+
+  const idToRemove = 4;
+  const result = await removeNote(idToRemove);
+  expect(result).toBeUndefined();
+});
