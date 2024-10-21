1f19abbdd1994fdd50b48ad56c24101a8e0072b8
Wamique Abro <wamik.abro212@gmail.com>
2024-10-21 16:38:30 +0500

    show 404 error page on page doesn't exist

--- ExpressJS/RESampleApp/index.js
+++ ExpressJS/RESampleApp/index.js
@@ -13,28 +13,28 @@ app.use("/staticfiles", express.static(path.join(__dirname, "assets")));
 
 // Regular Expression Approach
 app.get(/^(.+)$/, (req, res) => {
-  // fs.access(
-  //   path.join(__dirname, "src", `${req.params[0]}.html`),
-  //   fs.constants.F_OK,
-  //   (err) => {
-  //     // if file doesn't exist | error argument isn't empty
-  //     if (err) {
-  //       console.log("File doesn't exist");
-  //     } else {
-  //     }
-  //   }
-  // );
-
   if (req.params[0] == "/") {
     // or directly use
     // const requestedPath = req.params[0] === "/" ? "index" : req.params[0];
     res.sendFile(path.join(__dirname, "src/index.html"));
+  } else if (req.params[0] != "/") {
+    const filePath = path.join(__dirname, "src", `${req.params[0]}.html`);
+
+    // I can't wrap fs.access inside if statement because its async
+    // it will only work when we will apply the logic inside its callback function
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      // if file doesn't exist | error argument isn't empty
+      if (err) {
+        res.sendFile(path.join(__dirname, "src/404.html"));
+      } else {
+        // although params[0] is eg. /about-us
+        // concatinating inside join with comma would've made it eg. /about.us/.html
+        // because path.join automatically add / slashes in between two things being concatinated
+        // that's why we shouldn't concatinate but use this method:
+        res.sendFile(filePath);
+      }
+    });
   }
-  // although params[0] is eg. /about-us
-  // concatinating inside join with comma would've made it eg. /about.us/.html
-  // because path.join automatically add / slashes in between two things being concatinated
-  // that's why we shouldn't concatinate but use this method:
-  else res.sendFile(path.join(__dirname, "src", `${req.params[0]}.html`));
 });
 
 /* Separate approach is So Long
